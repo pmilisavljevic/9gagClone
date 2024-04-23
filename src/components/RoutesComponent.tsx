1a959a09a836e88a7208272c734f1d9f4761cf9c
Predrag Milisavljevic <pmilisavljevic@outlook.com>
2024-04-23 10:59:10 +0200

    routes fix

---
+++ src/components/RoutesComponent.tsx
@@ -0,0 +1,97 @@
+import { Route, Routes } from "react-router-dom";
+
+import EditProfile from "src/Pages/EditProfile/EditProfile";
+import LoginPage from "src/Pages/Login/LoginPage";
+import MainPage from "src/Pages/Main/MainPage";
+import MyFriends from "src/Pages/MyFriends/MyFriends";
+import MyPosts from "src/Pages/MyPosts/MyPosts";
+import NewPost from "src/Pages/NewPost/NewPost";
+import PostPage from "src/Pages/Post/PostPage";
+import PostsByFriend from "src/Pages/PostsByFriend/PostsByFriend";
+import PostsLikedByFriend from "src/Pages/PostsLikedByFriend/PostsLikedByFriend";
+import RequestsToMe from "src/Pages/RequestsToMe/RequestsToMe";
+import SignupPage from "src/Pages/Signup/SignupPage";
+import ProtectedRouteGuest from "src/utils/ProtectedRouteGuest";
+import ProtectedRouteUser from "src/utils/ProtectedRouteUser";
+
+export default function RoutesComponent() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route
+        path="login"
+        element={
+          <ProtectedRouteUser>
+            <LoginPage />
+          </ProtectedRouteUser>
+        }
+      />
+      <Route
+        path="signup"
+        element={
+          <ProtectedRouteUser>
+            <SignupPage />
+          </ProtectedRouteUser>
+        }
+      />
+      <Route
+        path="submit"
+        element={
+          <ProtectedRouteGuest>
+            <NewPost />
+          </ProtectedRouteGuest>
+        }
+      />
+
+      <Route path="/gag/:id" element={<PostPage />} />
+      <Route
+        path="edit-profile"
+        element={
+          <ProtectedRouteGuest>
+            <EditProfile />
+          </ProtectedRouteGuest>
+        }
+      />
+      <Route
+        path="requests-to-me"
+        element={
+          <ProtectedRouteGuest>
+            <RequestsToMe />
+          </ProtectedRouteGuest>
+        }
+      />
+      <Route
+        path="my-posts"
+        element={
+          <ProtectedRouteGuest>
+            <MyPosts />
+          </ProtectedRouteGuest>
+        }
+      />
+      <Route
+        path="my-friends"
+        element={
+          <ProtectedRouteGuest>
+            <MyFriends />
+          </ProtectedRouteGuest>
+        }
+      />
+      <Route
+        path="posts-by-friend/:id"
+        element={
+          <ProtectedRouteGuest>
+            <PostsByFriend />
+          </ProtectedRouteGuest>
+        }
+      />
+      <Route
+        path="posts-liked-by-friend/:id"
+        element={
+          <ProtectedRouteGuest>
+            <PostsLikedByFriend />
+          </ProtectedRouteGuest>
+        }
+      />
+    </Routes>
+  );
+}
